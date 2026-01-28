74623a3bcd3cdaf336fe9346ec0b750e1e71b30a
Md Obayet Khan <it24003@mbstu.ac.bd>
2026-01-28 15:15:43 +0600

    Refactor Exception class into DivisionProgram

--- Exception.java
+++ Exception.java
@@ -1,21 +1,30 @@
 import java.util.Scanner;
 import java.util.InputMismatchException;
 
-public class Exception {
+public class DivisionProgram {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int a = 0, b = 0;
 
         try {
-            int a = sc.nextInt();
-            int b = sc.nextInt();
+            System.out.print("Enter first integer: ");
+            a = sc.nextInt();
+
+            System.out.print("Enter second integer: ");
+            b = sc.nextInt();
+
             int result = a / b;
-            System.out.println(result);
-        } catch (ArithmeticException e) {
-            System.out.println("ArithmeticException occurred");
-        } catch (InputMismatchException e) {
-            System.out.println("InputMismatchException occurred");
-        } finally {
+            System.out.println("Result: " + result);
+        } 
+        catch (ArithmeticException e) {
+            System.out.println("Error: Division by zero is not allowed!");
+        } 
+        catch (InputMismatchException e) {
+            System.out.println("Error: Please enter valid integers!");
+        } 
+        finally {
             System.out.println("Program Execution Completed");
+            sc.close();
         }
     }
 }

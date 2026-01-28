a965d93565e16c88bd0a14311e9da09251cfd0f1
Md Obayet Khan <it24003@mbstu.ac.bd>
2026-01-28 15:16:56 +0600

    Refactor Employee classes with salary parameters
    Refactor Employee classes to include salary parameters for FullTime and PartTime employees. Update main method to instantiate employees with specific salary details.

--- Inheritance.java
+++ Inheritance.java
@@ -1,27 +1,43 @@
 class Employee {
-    double calculateSalary() {
-        return 0;
+    public double calculateSalary() {
+        return 0.0;
     }
 }
 
 class FullTimeEmployee extends Employee {
-    double calculateSalary() {
-        return 50000;
+    private double monthlySalary;
+
+    public FullTimeEmployee(double monthlySalary) {
+        this.monthlySalary = monthlySalary;
+    }
+
+    @Override
+    public double calculateSalary() {
+        return monthlySalary; 
     }
 }
 
 class PartTimeEmployee extends Employee {
-    double calculateSalary() {
-        return 20000;
+    private double hourlyRate;
+    private int hoursWorked;
+
+    public PartTimeEmployee(double hourlyRate, int hoursWorked) {
+        this.hourlyRate = hourlyRate;
+        this.hoursWorked = hoursWorked;
+    }
+
+    @Override
+    public double calculateSalary() {
+        return hourlyRate * hoursWorked; 
     }
 }
 
-public class Inheritance {
+public class Main {
     public static void main(String[] args) {
-        Employee e1 = new FullTimeEmployee();
-        Employee e2 = new PartTimeEmployee();
+        Employee e1 = new FullTimeEmployee(5000);
+        Employee e2 = new PartTimeEmployee(20, 120);
 
-        System.out.println(e1.calculateSalary());
-        System.out.println(e2.calculateSalary());
+        System.out.println("Full-time salary: " + e1.calculateSalary());
+        System.out.println("Part-time salary: " + e2.calculateSalary());
     }
 }
